ef83a6ad7eba506a447746bfdd2e6e74c285d0bc
JanhaviAlekar <janhavialekar@gmail.com>
2023-11-29 23:27:34 +0530

    CountDown ,sponsors Card

---
+++ src/components/AllSponsers.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import SponsorsCard from "./sponsorsCard";
+const AllSponsers =() =>{
+    return( <div className='col-md-9 text-center'>
+    <div className=' row row-cols-1 row-cols-md-3 g-4 '>
+            <div className="grid-container">
+            <div className="grid-item">
+            <div className="container-card">
+           <SponsorsCard/>
+           <SponsorsCard/>
+            </div>
+            </div>
+            </div>
+            </div>
+            </div>);
+}
+export default AllSponsers;
