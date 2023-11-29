ca094787f599d0404b685784c44b1fe63a6ff304
Ayush Ukhalkar <ayushuk98@gmail.com>
2023-11-29 13:32:53 +0530

    hero and navbar logos

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./hero.module.css";
-import img1 from '../../assets/homepage.jpg'
+import img1 from '../../assets/homepage.png';
+import img2 from '../../assets/mobile-image.png'; // Import your mobile image
 
 const Hero = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 724); // Set the breakpoint according to your needs
+    };
+
+    handleResize(); // Set initial screen size
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <>
       <div className={styles['banner-img']}>
-        <img src={img1} alt="Banner" />
+        <img src={isMobile ? img2 : img1} alt="Banner" />
       </div>
       <div className={styles['banner-text']}>
         <h2>Take the lead in style</h2>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import styles from './navbar.module.css'; // Import CSS module
-import { ReactComponent as Hamburger } from '../../assets/fast-food-burger-svgrepo-com.svg'
+import logo from '../../assets/logo.png';
+import { ReactComponent as Hamburger } from '../../assets/fast-food-burger-svgrepo-com.svg';
 import Hero from '../Hero/Hero';
-// import { ReactComponent as Brand } from '../../assets/icons/logo.svg'    
 
 const Navbar = () => {
   const [showNavbar, setShowNavbar] = useState(false);
@@ -12,40 +12,42 @@ const Navbar = () => {
     setShowNavbar(!showNavbar);
   };
 
-  return (<>
-    <nav className={styles.navbar}>
-      <div className='grad-bar'/>
-      <div className={styles.container}>
-        <div className={styles.logo}>
-          <img src="" alt="" />
+  return (
+    <>
+      <nav className={styles.navbar}>
+        <div className='grad-bar' />
+        <div className={styles.container}>
+          <div className={styles.logo}>
+            <img src={logo} alt="Logo" />
+          </div>
+          <div className={styles['menu-icon']} onClick={handleShowNavbar}>
+            <Hamburger className='ham' />
+          </div>
+          <div className={`${styles['nav-elements']} ${showNavbar ? styles.active : ''}`}>
+            <ul>
+              <li>
+                <NavLink to="/">Home</NavLink>
+              </li>
+              <li>
+                <NavLink to="/blog">Blog</NavLink>
+              </li>
+              <li>
+                <NavLink to="/projects">Projects</NavLink>
+              </li>
+              <li>
+                <NavLink to="/about">About</NavLink>
+              </li>
+              <li>
+                <NavLink to="/contact">Contact</NavLink>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div className={styles['menu-icon']} onClick={handleShowNavbar}>
-        <Hamburger className='ham'/>
-        </div>
-        <div className={`${styles['nav-elements']} ${showNavbar ? styles.active : ''}`}>
-          <ul>
-            <li>
-              <NavLink to="/">Home</NavLink>
-            </li>
-            <li>
-              <NavLink to="/blog">Blog</NavLink>
-            </li>
-            <li>
-              <NavLink to="/projects">Projects</NavLink>
-            </li>
-            <li>
-              <NavLink to="/about">About</NavLink>
-            </li>
-            <li>
-              <NavLink to="/contact">Contact</NavLink>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-    <Hero/>
+      </nav>
+      <Hero />
     </>
   );
 };
 
 export default Navbar;
+
