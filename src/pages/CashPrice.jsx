28bd74bc2effa230734f9eaee9febf05c2e5d203
Ayush Ukhalkar <ayushuk98@gmail.com>
2024-01-14 08:55:39 +0530

    tables updated

--- src/pages/CashPrice.jsx
+++ src/pages/CashPrice.jsx
@@ -9,32 +9,32 @@ const CashPrice = () =>{
 <div className="ALL-Pages-height">
     <div className="tables-conatiner">
       <div className="table-wrapper">
-      <h1 className="table-title">15 years and older</h1>
+      <h1 className="table-title">Cash Prizes</h1>
     <div className="cashprize">
 <table>
       <thead>
         <tr>
           <th>Prize</th>
-          <th>Male</th>
-          <th>Female</th>
+          <th>Male/Female(10 to 14 Years)</th>
+          <th>Male/Female(14 to 18 Years)</th>
         </tr>
       </thead>
       <tbody>
         <tr>
           <td>1st</td>
-          <td>Rs.10000</td>
-          <td>Rs.10000</td>
+          <td>Rs.5000</td>
+          <td>Rs.5000</td>
         </tr>
         <tr>
           <td>2nd</td>
-          <td>Rs.5000</td>
-          <td>Rs.5000</td>
+          <td>Rs.3000</td>
+          <td>Rs.3000</td>
 
         </tr>
         <tr>
           <td>3rd</td>
-          <td>Rs.3000</td>
-          <td>Rs.3000</td>
+          <td>Rs.2000</td>
+          <td>Rs.2000</td>
 
         </tr>
         <tr>
@@ -45,33 +45,32 @@ const CashPrice = () =>{
     </div>
     </div>
     <div className="table-wrapper">
-      <h1 className="table-title">Under 15 years</h1>
    <div className="cashprize">
 
     <table >
       <thead>
         <tr>
         <th>Prize</th>
-          <th>Male</th>
-          <th>Female</th>
+          <th>Male/Female(18 to 25 Years)</th>
+          <th>FUN RUN</th>
         </tr>
       </thead>
       <tbody>
         <tr>
           <td>1st</td>
-          <td>Rs.10000</td>
-          <td>Rs.10000</td>
+          <td>Rs.5000</td>
+          <td>Rs.3000</td>
         </tr>
         <tr>
           <td>2nd</td>
-          <td>Rs.5000</td>
-          <td>Rs.5000</td>
+          <td>Rs.3000</td>
+          <td>Rs.1000</td>
 
         </tr>
         <tr>
           <td>3rd</td>
-          <td>Rs.3000</td>
-          <td>Rs.3000</td>
+          <td>Rs.2000</td>
+          <td>Rs.1000</td>
 
         </tr>
       </tbody>
